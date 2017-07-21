9deaff6a64bf192431ac6cdeb1d39b60f9a8ad58
Markin Andrey <magazon4ik@yandex.ru>
2017-07-21 18:39:34 +0300

    changed

---
+++ app/src/main/java/com/development/markin/photogallery/models/StartupReceiver.java
@@ -0,0 +1,18 @@
+package com.development.markin.photogallery.models;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by magaz on 21.07.2017.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    public static final String TAG = "StartupReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: "+intent.getAction());
+    }
+}
